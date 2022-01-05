0d0d353e29c63824dd26fe725d9352f6e62581c0
Osei Pinamang Kwadwo <osei.pinamang@gmail.com>
2022-01-05 14:13:37 +0000

    Add substring.rb

--- substring.rb
+++ substring.rb
@@ -2,7 +2,7 @@ dictionary = %w[below down go going horn how howdy it i low own part partner sit
 
 def substring(string, dictionary)
     result = Hash.new(0)  #creating empty hash
-    lowered_string = string.downcase! #converting strings to downcase
+    lowered_string = string.downcase! #converting strings to downcase permanently
     dictionary.each do |word|    #loop through words in the dictionary
         matches = lowered_string.scan(word).length   #scan string for occurences
         result[word]= matches unless matches == 0   #add values to hash
